42dec7b8d5e3ede8e2256ade48526febda1bbe1d
nahid270 <110238003+nahid270@users.noreply.github.com>
2024-03-21 21:28:20 +0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,9 +4,9 @@ then
   git clone https://github.com/nahid270/Free-Filter-Bot / Free-Filter-Bot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /AutoFilterAdvance
+  git clone $UPSTREAM_REPO / Free-Filter-Bot
 fi
-cd /AutoFilterAdvance
+cd / Free-Filter-Bot
 pip3 install -U -r requirements.txt
 echo "Bot Started...."
 python3 bot.py
